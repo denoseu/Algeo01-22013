bccf39797d536e396c9384b6e3fba6f70dad35ea
angiekierra <angie.kierra@gmail.com>
2023-10-05 04:50:19 +0700

    minor changes

--- src/Matrix/outputFile.java
+++ src/Matrix/outputFile.java
@@ -4,7 +4,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.DecimalFormat;
 
-import javax.swing.plaf.basic.BasicBorders.SplitPaneBorder;
 
 import src.Functions.*;
 import src.Matrix.*;
