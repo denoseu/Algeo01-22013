d621b5574bc91d61d1946456fc5cee52747ec577
denoseu <denisefeliciatiowanni@gmail.com>
2023-10-05 23:09:21 +0700

    update main

--- run.bat
+++ run.bat
@@ -1,10 +1,9 @@
 cls
 
-javac -d bin\ .\src\Main\main.java .\src\Functions\* .\src\Matrix\* && jar cvmf MANIFEST.MF Algeo01-22013.jar bin\src\main.class bin\src\Functions\* bin\src\Matrix\*  && java -Kaydenji.jar
+javac -d bin\ .\src\Main\main.java .\src\Functions\* .\src\Matrix\* && jar cvmf MANIFEST.MF Algeo01-22013.jar bin\src\Main\* bin\src\Functions\* bin\src\Matrix\*  && java -Algeo01-22013.jar
+
 :: untuk compile semua file di source (src) menjadi binary (bin)
 
+:: output berupa Algeo01-22013.jar, butuh file manifest dan class dari bin
 
-:: output berupa Kaydenji.jar, butuh file manifest dan class dari bin
-
-
-:: running file Kaydenji.jar
+:: running file Algeo01-22013.jar
