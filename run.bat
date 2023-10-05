a29db7506270298e6c5461b1702002ac0aa59aa1
angiekierra <angie.kierra@gmail.com>
2023-10-05 23:24:45 +0700

    final semoga

--- run.bat
+++
@@ -1,9 +0,0 @@
-cls
-
-javac -d bin\ .\src\Main\main.java .\src\Functions\* .\src\Matrix\* && jar cvmf MANIFEST.MF Algeo01-22013.jar bin\src\Main\* bin\src\Functions\* bin\src\Matrix\*  && java -Algeo01-22013.jar
-
-:: untuk compile semua file di source (src) menjadi binary (bin)
-
-:: output berupa Algeo01-22013.jar, butuh file manifest dan class dari bin
-
-:: running file Algeo01-22013.jar
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-clear
-
-javac -d bin/ $(find ./src/* | grep .java) && jar cvmf MANIFEST.MF Algeo01-22013.jar  bin/src/* && java -jar Algeo01-22013.jar
